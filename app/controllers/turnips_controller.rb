cb870b53712cd1497ecaf37efe041450b5419729
onokusuna <piroshki4315@gmail.com>
2020-05-20 22:56:12 +0900

    applicaton.scssへ変更 株価投稿フォーム基礎作成

--- app/controllers/turnips_controller.rb
+++ app/controllers/turnips_controller.rb
@@ -1,4 +1,22 @@
 class TurnipsController < ApplicationController
   def index
+  	@turnips = Turnip.all
+  	@turnip = Turnip.new
   end
+
+  def create
+  	@turnip = Turnip.new
+  	if @turnip.save
+  	   redirect_to turnips_path, notice: '投稿に成功しました'
+  	else
+  	   @turnips = Turnip.all
+  	   flash.now[:notice] = '投稿に失敗しました'
+  	   render :index
+  	end
+  end
+
+  private
+  	def turnip_params
+      params.require(:turnip).permit(:name, :price, :password, :want)
+    end
 end
