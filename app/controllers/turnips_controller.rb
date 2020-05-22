cd3fa0c17a829c45bc665efb81447898d5a37651
onokusuna <piroshki4315@gmail.com>
2020-05-22 15:59:35 +0900

    株価投稿に過去の時間を登録できないようにバリデーション追加

--- app/controllers/turnips_controller.rb
+++ app/controllers/turnips_controller.rb
@@ -8,11 +8,11 @@ class TurnipsController < ApplicationController
   def create
   	@turnip = Turnip.new(turnip_params)
   	if @turnip.save
-  	   redirect_to turnips_path, success: '投稿に成功しました'
+  	  redirect_to turnips_path, success: "投稿に成功しました"
   	else
-  	   @turnips = Turnip.all
-  	   flash.now[:danger] = '投稿に失敗しました'
-  	   render :index
+  	  @turnips = Turnip.all
+  	  flash.now[:danger] = "投稿に失敗しました"
+  	  render :index
   	end
   end
 
--- app/models/turnip.rb
+++ app/models/turnip.rb
@@ -5,4 +5,11 @@ class Turnip < ApplicationRecord
   validates :want,        presence: true, length: { in: 3..100 }
   validates :started_at,  presence: true
   validates :finished_at, presence: true
+  # 自作バリデーション 現在時間よりも早いものは登録できない
+  validate  :time_not_before_now
+
+  def time_not_before_now
+    errors.add(:started_at, "現在時刻より後の時刻を選択してください") if started_at.nil? || started_at < Time.current.ago(30.minutes)
+    errors.add(:finished_at, "現在時刻より後の時刻を選択してください") if finished_at.nil? || finished_at < Time.current
+  end
 end
