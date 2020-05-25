5d8f7c9578a3273fedd346cb1a967a301bebb0a2
onokusuna <piroshki4315@gmail.com>
2020-05-25 15:58:25 +0900

    refile画像が表示されるように追記 sample_imageをデータがない場合は表示されないように記述

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'cb156cdf91ed6282f22b3a34f021dbc0059dc8d451155cca8cf1489e7729b5a654bf38de20b77b5a8e1b992d9b0747da4e1f05d6325f95982d9edb6dcbef6b86'
